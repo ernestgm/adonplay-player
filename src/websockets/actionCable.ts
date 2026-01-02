22c4a42b672310faa515704eed868055984b33de
ernes <ernestgm2006@gmail.com>
2026-01-02 11:57:09 -0500

    Fixed Cable Error!

--- src/websockets/actionCable.ts
+++ src/websockets/actionCable.ts
@@ -1,7 +1,8 @@
 // src/utils/actionCable.js (O donde manejes tu lógica de Action Cable)
-import { createConsumer } from "@rails/actioncable";
+import { createConsumer, logger } from "@rails/actioncable";
 import Cookies from "js-cookie";
 
+logger.enabled = false;
 const deviceId= Cookies.get("device_id");
 const cableUrl = `${process.env.NEXT_PUBLIC_PLAYER_RAILS_ACTION_CABLE_URL}?device_id=${deviceId}` || `ws://ws-adonplay.local/cable?device_id_id=${deviceId}`;
 const cable = createConsumer(cableUrl);
