4b1122c984a7518de2884dbc38c50f06eacb9889
ernes <ernestgm2006@gmail.com>
2026-01-17 17:56:18 -0500

    Fixed Chromiun

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import {SidebarProvider} from '@/context/SidebarContext';
 import {ThemeProvider} from '@/context/ThemeContext';
 import {ErrorProvider} from "@/context/ErrorContext";
 import {Suspense, useEffect} from "react";
+import Script from "next/dist/client/script";
 
 export default function RootLayout({
                                        children,
@@ -50,6 +51,20 @@ export default function RootLayout({
         <html lang="en">
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+            <Script id="emergency-polyfills" strategy="beforeInteractive">{
+                "if (typeof window.AbortController === 'undefined') {\n"+
+                "              console.log('Patching AbortController...');\n"+
+                "              // Script mínimo para evitar el crash inmediato\n"+
+                "              window.AbortController = function() {\n"+
+                "                return { signal: {}, abort: function() {} };\n"+
+                "              };\n"+
+                "            }\n"+
+                "                if (typeof globalThis === 'undefined') {\n"+
+                "                  window.globalThis = window;\n"+
+                "                }\n"+
+                "            }"
+            }
+            </Script>
         </head>
         <body className={`dark:bg-gray-900`}>
         <ThemeProvider>
